e42b38e039f6075a3f4495a6f95639afcd661c8b
Pathmila <asinipathmila@gmail.com>
2021-07-22 04:05:54 +0530

    routers

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name: "Asini",
+        email: 'asinipathmila@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Nisini',
+        email: 'nisininethmila@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Sandamini',
+        email: 'sandamni@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Sumith',
+        email: 'sumith@gmail.com'
+    }
+]
+
+module.exports = users
